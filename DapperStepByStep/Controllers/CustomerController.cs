53000a7b86e92e3b8bd2314df4d4a37c17b3d00e
Rodion Ivanov <rodion.ivanov999@gmail.com>
2022-02-23 21:32:23 +0600

    Dapper with MVC

---
+++ DapperStepByStep/Controllers/CustomerController.cs
@@ -0,0 +1,134 @@
+﻿using Dapper;
+using DapperStepByStep.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DapperStepByStep.Controllers
+{
+    public class CustomerController : Controller
+    {
+        readonly string connectionString = "Server=.;Database=DapperStepByStep;Trusted_Connection=True;";
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                customers = db.Query<Customer>("Select * From Customers").ToList();
+            }
+            return View(customers);
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            Customer customer = new Customer();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                customer = db.Query<Customer>("Select * From Customers WHERE CustomerID =" + id, new { id }).SingleOrDefault();
+            }
+            return View(customer);
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "Insert Into Customers (FirstName, LastName, Email) Values(@FirstName, @LastName, @Email)";
+
+                    int rowsAffected = db.Execute(sqlQuery, customer);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Customer customer = new Customer();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                customer = db.Query<Customer>("Select * From Customers WHERE CustomerID =" + id, new { id }).SingleOrDefault();
+            }
+            return View(customer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "UPDATE Customers set FirstName='" + customer.FirstName +
+                        "',LastName='" + customer.LastName +
+                        "',Email='" + customer.Email +
+                        "' WHERE CustomerID=" + customer.CustomerID;
+
+                    int rowsAffected = db.Execute(sqlQuery);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Customer customer = new Customer();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                customer = db.Query<Customer>("Select * From Customers WHERE CustomerID =" + id, new { id }).SingleOrDefault();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "Delete From Customers WHERE CustomerID = " + id;
+
+                    int rowsAffected = db.Execute(sqlQuery);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
